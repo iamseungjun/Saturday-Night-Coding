deb8123d9d49f9b5741505efef21e84d9f268cc4
Seung Jun Lee (이승준) <55793046+iamseungjun@users.noreply.github.com>
2020-03-07 00:10:04 +0900

    Create deletemember.php

---
+++ deletemember.php
@@ -0,0 +1,14 @@
+<?php
+    $conn = mysqli_connect(
+        'localhost',
+        'root',
+        'E9LWMIZotVGX',
+        'contest'
+    );
+
+    $sql = "DELETE FROM member WHERE id='{$_GET['id']}'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result);
+?>
+<meta charset="utf-8">
+<script type="text/javascript">alert('삭제 완료'); location.href = "admin.php"; </script>
