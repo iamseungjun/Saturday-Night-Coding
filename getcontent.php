a580bc30c7cb3e63817026334912fa03143ad40e
Seung Jun Lee (이승준) <55793046+iamseungjun@users.noreply.github.com>
2020-03-06 22:48:29 +0900

    Update getcontent.php

--- getcontent.php
+++ getcontent.php
@@ -12,6 +12,8 @@
         $row = mysqli_fetch_array($result);
 
         $str = "<li>".$row['content1']."</li><li>".$row['content2']."</li><li>".$row['content3']."</li><li>".$row['content4']."</li><li>".$row['content5']."</li><li>".$row['content6']."</li><li>".$row['content7']."</li>";
+
+        echo '<li>'.$row['content1'].'</li>';
         echo $str;
     }
 ?>
