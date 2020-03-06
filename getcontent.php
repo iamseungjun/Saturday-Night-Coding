b22bc2a7dddaba4675a6d903964af20b2201f268
Seung Jun Lee (이승준) <55793046+iamseungjun@users.noreply.github.com>
2020-03-06 22:22:35 +0900

    Update getcontent.php

--- getcontent.php
+++ getcontent.php
@@ -10,9 +10,7 @@
         $sql = "SELECT * FROM application WHERE id='{$_POST['identy']}'";
         $result = mysqli_query($conn, $sql);
         $row = mysqli_fetch_array($result);
-        
-        echo "$_POST['identy']";
-        echo "DATA TEST !!!!!!!!!! PLZ\n";
+
         echo "<ol><li>".$row['content1']."</li><li>".$row['content2']."</li><li>".$row['content3']."</li><li>".$row['content4']."</li><li>".$row['content5']."</li><li>".$row['content6']."</li><li>".$row['content7']."</li></ol>";
     }
 ?>
