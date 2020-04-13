c59b3d170d0975608e2ad34070122feef221467b
iamseungjun <leezeunjun@gmail.com>
2020-04-13 22:17:08 +0900

    Update

--- modify_ok.php
+++ modify_ok.php
@@ -9,8 +9,27 @@
     $id = $_SESSION['id'];
     $passwd = password_hash($_POST['pw'], PASSWORD_DEFAULT);
     $email = $_POST['email'];
+    $facebook = $_POST['facebook'];
+    $twitter = $_POST['twitter'];
+    $instagram = $_POST['instagram'];
+    $github = $_POST['github'];
+    $youtube = $_POST['youtube'];
+    $discord = $_POST['discord'];
+    $codeforces = $_POST['codeforces'];
+    $atcoder = $_POST['atcoder'];
 
-    $sql = "UPDATE member SET pw='{$passwd}', email='{$email}' WHERE id='{$id}'";
+    $sql = "UPDATE member SET
+        pw='{$passwd}',
+        email='{$email}',
+        facebook='{$facebook}',
+        twitter='{$twitter}',
+        instagram='{$instagram}',
+        github='{$github}',
+        youtube='{$youtube}',
+        discord='{$discord}',
+        codeforces='{$codeforces}',
+        atcoder='{$atcoder}'
+    WHERE id='{$id}'";
     $result = mysqli_query($conn, $sql);
     $_SESSION['id'] = $id;
 ?>
